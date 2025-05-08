9ee6515b93799dbb9c3228fee2dd74801b58b883
Ethan Gardner <ethangardner@users.noreply.github.com>
2025-05-08 15:44:41 -0400

    Add purgecss config to project (#24)
    * add purgecss config to project
    
    * move dependency to dev

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,8 +1,53 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import purgecss from 'astro-purgecss';
+import { join as pathJoin, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    // purgecss should go last
+    purgecss({
+      fontFace: true,
+      keyframes: true,
+      safelist: {
+        standard: [
+            /abbr/,
+            "kbd",
+            "samp",
+            "sub",
+            "optgroup",
+            "fieldset",
+            "summary",
+            "cite",
+            "dfn",
+            "pre",
+        ],
+        deep: [
+            /usa-in-page.+/
+        ]
+      },
+      dynamicAttributes: [
+          "contentEditable",
+          "title",
+          "type"
+      ],
+      blocklist: [],
+      content: [
+        pathJoin(__dirname, 'src/**/*.{astro,svelte,ts,tsx,js,jsx}'),
+        pathJoin(__dirname, '../../packages/ui', '/src/**/*.{svelte,js,jsx}'),
+      ],
+      extractors: [
+        {
+          extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          extensions: ['astro', 'html']
+        }
+      ]
+    })],
 });
